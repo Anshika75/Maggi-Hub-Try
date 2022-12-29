21ba6c586ca84e8b4c09c850b0f83fe6a21e1e3e
Anshika <anshikathakur596@gmail.com>
2022-12-29 21:48:08 +0530

    menulist updated

--- src/components/SubMenuCard.jsx
+++ src/components/SubMenuCard.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 // import { Link } from "react-router-dom";
 // import ItemBox from "./ItemBox";
 export default function SubMenuCard(props) {
@@ -19,20 +19,22 @@ export default function SubMenuCard(props) {
       </div>
       {
         isOpen ? (
-          <div className="absolute z-20 h-[250px] w-[500px] bg-[#FFFFFF] shadow-[0_0_4px_1px_#e3e3e37c] flex flex-row justify-center items-center ">
+          <div className="absolute grid place-items-center h-full w-full z-20">
+          <div className="relative h-[250px] w-[500px] bg-[#FFFFFF] shadow-[0_0_4px_1px_#e3e3e37c] flex flex-row justify-center items-center ">
             <div className="w-[40%] h-full grid place-items-center overflow-hidden">
               <img className="w-full h-full hover:scale-125 transition-all hover:rotate-6" src={props.img} alt="" />
             </div>
             <div className="w-[60%] h-full flex flex-col justify-start items-start">
               <div className="text-2xl tracking-wide text-orange font-Dosis mt-5 mx-3 font-semibold opacity-95 text-center">{props.title}</div>
               <div className="text-xl tracking-wide text-grey font-Dosis mx-3 font-semibold opacity-75 text-center">{props.cost}</div>
-              <div className="text-lg text-grey font-Dosis mt-5 mx-3 font-light opacity-75">{props.desc}</div>
+              <div className="text-lg text-grey font-Dosis mt-3 mx-3 font-normal">{props.desc}</div>
             </div>
-            <div  onClick={() => setIsOpen(false)} className="absolute top-0 right-0 h-8 w-8 cursor-pointer grid place-items-center">
-            <i className="fa-solid fa-xmark text-grey text-2xl grid place-items-center transition-all opacity-75 hover:opacity-100"></i>
+            <div onClick={() => setIsOpen(false)} className="absolute top-0 right-0 h-8 w-8 cursor-pointer grid place-items-center">
+              <i className="fa-solid fa-xmark text-grey text-2xl grid place-items-center transition-all opacity-75 hover:opacity-100"></i>
             </div>
-      </div>
-        ) : null 
+          </div>
+          </div>
+        ) : null
       }
     </>
   );
--- src/components/SubMenuCardBox.jsx
+++ src/components/SubMenuCardBox.jsx
@@ -8,7 +8,7 @@ export default function SubMenuCardBox(props) {
       <div className="flex flex-col lg:flex-row flex-wrap justify-evenly items-center w-full p-3 pb-0 relative">
         {
           props.SubMenuListArray.SubMenu?props.SubMenuListArray.SubMenu.map((e, k) => {
-          return <SubMenuCard title={e.title} img={e.img} path={"/"+e.title} key={k}/>;
+          return <SubMenuCard title={e.title} img={e.img} desc={e.desc} cost={e.cost} path={"/"+e.title} key={k}/>;
         }):<div className="text-2xl tracking-wide text-grey font-Dosis mt-5 mx-3 font-semibold opacity-95 text-center">No {props.SubMenuListArray.title} Found</div>
         }
 
